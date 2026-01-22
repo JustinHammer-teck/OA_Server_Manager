59a003e60f0bfd925219c276b94687f1f087ac1d
Huy <huy.quoc1205@gmail.com>
2026-01-22 16:46:13 +1100

    revert files

--- test.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-from core.server.server import tail_log
-
-log_path = r"D:\HuyDinh\arena\openarena-0.8.8\openarena-0.8.8\baseoa\qconsole.log"
-
-for line in tail_log(log_path):
-    print("[SERVER LOG]", line)
